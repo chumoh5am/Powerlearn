dfe88430b5706bbfa731db03cfb8407a4dc12bc6
chumoh5am <56386621+chumoh5am@users.noreply.github.com>
2023-02-26 08:28:25 +0000

    DataTypes in Dart

---
+++ dataTypes.dart
@@ -0,0 +1,43 @@
+//Dart data types examples
+//Integers
+// Expresses numerical formats
+void main(){  
+{
+  int Dob = 1998;
+  print("I was born in $Dob");
+} 
+
+//Double example
+ // Double are used to demonstrate numbers with decimal points
+
+{
+  double x=2.5;
+double y=3.1;
+double z=x+y;
+  print(z);
+};
+
+//String Example
+// Code to print strings in dart
+{
+  String fName = "Sammy";
+  String lName = " Chumoh";
+  print("My name is " + fName + lName);
+};
+
+// List example
+//Expresses how list is used in dart
+{
+  var osModel = ["Windows", "Mac", "Linux"];
+  print(osModel);
+} 
+
+// Map in dart
+// Map has values/keys pairs and can be of any data type
+{
+  var details = {'Username':'admin','Password':'pass@123'}; 
+   print(details);
+}   
+}
+
+
