f025068aded74bc27816a015096049ee34baffef
chumoh5am <56386621+chumoh5am@users.noreply.github.com>
2023-03-06 05:15:41 +0000

    functions in dart

---
+++ functions.dart
@@ -0,0 +1,56 @@
+
+ // Function that adds two numbers
+ void addTwo(int num1, int num2){
+  int sum = num1 + num2;
+   print("The sum is $sum");
+}
+
+void main(){
+  add(40, 20);
+}
+
+// function that find the difference between two numbers
+void subtractTwo(int num1, int num2){
+  int difference = num1 - num2;
+  print("The difference is $difference");
+}
+void main(){
+  subtractTwo(40,20);
+} 
+
+// function that finds the product between two numbers
+void multiplyTwo(int num1, int num2){
+  int product = num1 * num2;
+  print("The product is $product");
+}
+void main(){
+  multiplyTwo(40,20);
+}  
+
+// function that divides two numbers
+void divideTwo(num1, num2){
+   var divide = num1 / num2;
+  print("The division is $divide");
+}
+void main(){
+  divideTwo(40,20);
+}  
+
+void main(){
+    //declaring s tring variable
+     String example = 'I love dart programming';
+    //get string length
+    int length = example.length;
+     //printing the length of the string
+    print(length);
+    }
+    
+
+void main(){
+  //declare a list
+  var list = ["Dart", "Python", "Javascript", "Php"];
+  //select the index to print
+  var element = list.elementAt(0);
+  //print the selected element above
+  print('Element: $element');
+}
